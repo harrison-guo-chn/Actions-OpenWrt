59d39d6a3c00024207c545e288c35a8d082cb7d4
harrison-guo-chn <58265745+harrison-guo-chn@users.noreply.github.com>
2024-08-02 00:07:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,19 +16,28 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# TTYD 免登录
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-
 # 移除要替换的包
 rm -rf feeds/smpackage/luci-app-wechatpush
+rm -rf feeds/smpackage/luci-app-filebrowser
 find feeds/smpackage -type d -name 'luci-theme*' -exec rm -rf {} \;
 find feeds/smpackage -type d -name 'luci-app-argon*' -exec rm -rf {} \;
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
 
 # 添加额外插件
 git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatpush
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
